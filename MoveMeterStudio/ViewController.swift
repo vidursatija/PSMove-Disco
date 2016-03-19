0f9faf2ad806e2668600448cbd8a8e4e76bd91c3
vidursatija <vidursatija@gmail.com>
2016-03-19 12:48:46 +0530

    Initial Commit

---
+++ MoveMeterStudio/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MoveMeterStudio
+//
+//  Created by Vidur Satija on 19/03/16.
+//  Copyright © 2016 Aromatic Studios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
